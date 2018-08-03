5623173b221e2154b75f8f551f6b9b4ff695d1df
lzcykevin <lzcykevin@hotmail.com>
2018-08-03 10:47:13 +0800

    :bug: fix bug

--- setrepost.sh
+++ setrepost.sh
@@ -355,7 +355,9 @@ install_iptables_haProxy(){
 	else
 		apt-get update && apt-get install -y vim haproxy
 	fi
-	chmod +x /etc/rc.local
+	if [[ -f /etc/rc.local ]]; then
+		chmod +x /etc/rc.local
+	fi
 	HaProxy_exist=`haproxy -v`
 	if [[ ${HaProxy_exist} = "" ]]; then
 		echo -e "\033[41;37m [错误] \033[0m 安装HaProxy失败，请检查 !" && exit 1
