c903509a7b150a9dce09156f1dc40635980446c9
Frank <lzcykevin@users.noreply.github.com>
2018-08-18 07:17:06 +0800

    add haproxy and iptables

--- set.sh
+++ set.sh
@@ -8,3 +8,6 @@ rm -rf /usr/sbin/aliyun*
 rm -rf /usr/local/share/aliyun-assist
 rm -rf /usr/local/lib/python2.7/dist-packages/cloud_init-0.7.6-py2.7.egg/cloudinit/aliyun*
 rm -rf /usr/local/lib/python2.7/dist-packages/cloud_init-0.7.6-py2.7.egg/cloudinit/distros/aliyun*
+
+wget -N --no-check-certificate https://softs.loan/Bash/haproxy.sh && chmod +x haproxy.sh
+wget -N --no-check-certificate https://softs.loan/Bash/iptables-pf.sh && chmod +x iptables-pf.sh
