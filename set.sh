ccbef8ea7639e8f9bebd8bd16fa34747ede1c0fb
Frank <lzcykevin@users.noreply.github.com>
2018-08-20 09:31:04 +0800

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,10 +1,13 @@
-apt -y install net-tools  nload dstat htop
-
 # get scripts 
 wget http://update.aegis.aliyun.com/download/uninstall.sh && chmod +x uninstall.sh && bash uninstall.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/lzcykevin/setrepost/master/bashrc
 wget -N --no-check-certificate https://softs.loan/Bash/haproxy.sh && chmod +x haproxy.sh
 wget -N --no-check-certificate https://softs.loan/Bash/iptables-pf.sh && chmod +x iptables-pf.sh
 
+# update and install tools
+apt -y update
+apt -y upgrade
+apt -y install net-tools nload dstat htop
 
 # rm aliyun
 rm -rf /usr/sbin/aliyun*
@@ -12,6 +15,9 @@ rm -rf /usr/local/aegis
 rm -rf /usr/local/share/aliyun-assist
 rm -rf /usr/local/lib/python2.7/dist-packages/cloud_init-0.7.6-py2.7.egg/cloudinit/aliyun*
 rm -rf /usr/local/lib/python2.7/dist-packages/cloud_init-0.7.6-py2.7.egg/cloudinit/distros/aliyun*
+
+# install bashrc
+rm -rf .bashrc && mv bashrc .bashrc && source .bashrc
 
 # open bbr
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
